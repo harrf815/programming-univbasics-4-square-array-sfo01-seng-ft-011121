9dff9d5655950b2a39413a8eb9e628303ccda394
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-24 00:01:50 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   index = 0 
-    while index <= array.length do  
+    while index >= array.length do  
     new_array.push(array[index].sqrt)
     index += 1 
   end
